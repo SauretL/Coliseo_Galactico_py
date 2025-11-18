b033d1e221bae7d2dda1f9a660e06792da22b1e6
Sauret <sauret.lucas.ivan@gmail.com>
2025-11-17 23:28:24 -0300

    hice un enemigo y un personaje jugador. Los puse como diccionarios en archivos separados

---
+++ gladiadores/enemigos.py
@@ -0,0 +1,151 @@
+enemigo_azul = {
+      "id": 4,
+      "nombre": "Azul",
+      "puntos de acción" : 4,
+      "precisión por armas": {
+            "espada": 0,
+            "hacha": 0,
+            "daga": 0,
+            "martillo": 0
+      },
+      "afinidades elementales" : {
+            "fuego" : 0,
+            "hielo" : 0,
+            "electricidad" : 0,
+            "viento" : 0,
+            "naturaleza" : 0,
+            "gravedad" : 0
+      },
+      "zonas": {
+            "cabeza": {
+                  "zona vital única": True,
+                  "zona vital colectiva"  : False,
+                  "salud" : 18,
+                  "evasión" : 45,
+                  "armadura" : 0,
+                  "equipa armas" : False,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "torso": {
+                  "zona vital única": True,
+                  "zona vital colectiva"  : False,
+                  "salud" : 30,
+                  "evasión" : 25,
+                  "armadura" : 0,
+                  "equipa armas" : False,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "brazo derecho": {
+                  "zona vital única": False,
+                  "zona vital colectiva"  : False,
+                  "salud" : 15,
+                  "evasión" : 30,
+                  "armadura" : 0,
+                  "equipa armas" : True,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "brazo izquierdo": {
+                  "zona vital única": False,
+                  "zona vital colectiva"  : False,
+                  "salud" : 15,
+                  "evasión" : 30,
+                  "armadura" : 0,
+                  "equipa armas" : True,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "piernas": {
+                  "zona vital única": False,
+                  "zona vital colectiva"  : False,
+                  "salud" : 15,
+                  "evasión" : 40,
+                  "armadura" : 0,
+                  "equipa armas" : False,
+                  "habilidades": ["(pasiva) +10 evasión a sus otras zonas"],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            }
+      }
+      }
---
+++ gladiadores/pjs.py
@@ -0,0 +1,151 @@
+jugador_luna = {
+      "id": 1,
+      "nombre": "Luna",
+      "puntos de acción" : 4,
+      "precisión por armas": {
+            "espada": 0,
+            "hacha": 0,
+            "daga": 0,
+            "martillo": 0
+      },
+      "afinidades elementales" : {
+            "fuego" : 1,
+            "hielo" : 1,
+            "electricidad" : 1,
+            "viento" : 1,
+            "naturaleza" : 1,
+            "gravedad" : 1
+      },
+      "zonas": {
+            "cabeza": {
+                  "zona vital única": True,
+                  "zona vital colectiva"  : False,
+                  "salud" : 18,
+                  "evasión" : 45,
+                  "armadura" : 0,
+                  "equipa armas" : False,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "torso": {
+                  "zona vital única": True,
+                  "zona vital colectiva"  : False,
+                  "salud" : 30,
+                  "evasión" : 25,
+                  "armadura" : 0,
+                  "equipa armas" : False,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "brazo derecho": {
+                  "zona vital única": False,
+                  "zona vital colectiva"  : False,
+                  "salud" : 15,
+                  "evasión" : 30,
+                  "armadura" : 0,
+                  "equipa armas" : True,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "brazo izquierdo": {
+                  "zona vital única": False,
+                  "zona vital colectiva"  : False,
+                  "salud" : 15,
+                  "evasión" : 30,
+                  "armadura" : 0,
+                  "equipa armas" : True,
+                  "habilidades": [],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            },
+            "piernas": {
+                  "zona vital única": False,
+                  "zona vital colectiva"  : False,
+                  "salud" : 15,
+                  "evasión" : 40,
+                  "armadura" : 0,
+                  "equipa armas" : False,
+                  "habilidades": ["(pasiva) +10 evasión a sus otras zonas"],
+                  "resistencias": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  },
+                  "debilidades": {
+                        "fuego" : False,
+                        "hielo" : False,
+                        "electricidad" : False,
+                        "viento" : False,
+                        "naturaleza" : False,
+                        "gravedad" : False
+                  }
+                      
+            }
+      }
+      }
